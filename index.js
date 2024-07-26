61245929bb1d0d7d39325a4c9d0518d747ebd301
Zoro D. Great <72684934+ZoroPinus@users.noreply.github.com>
2024-07-26 22:09:57 +0800

    update prisma

--- index.js
+++ index.js
@@ -19,20 +19,20 @@ io.on("connection", (socket) => {
   console.log("User Connected");
 
   // Listen for the event to get smokeLevel based on sensorId
-  socket.on("getSmokeLevel", async (sensorId) => {
+  socket.on("getSmokeConcentration", async (sensorId) => {
     clients[socket.id] = sensorId; // Store the sensorId for this client
 
     try {
-      // Query the database for the smoke level of the given sensorId
+      // Query the database for the smoke Concentration of the given sensorId
       const lastReading = await prisma.lastReading.findUnique({
         where: { sensorId: sensorId },
-        select: { smokeLevel: true },
+        select: { smokeConcentration: true },
       });
 
       if (lastReading) {
         // Emit the smokeLevel back to the client
-        console.log(lastReading.smokeLevel);
-        socket.emit("smokeLevel", lastReading.smokeLevel);
+        console.log(lastReading.smokeConcentration);
+        socket.emit("smokeConcentration", lastReading.smokeConcentration);
       } else {
         // Emit a message indicating no data was found
         socket.emit("error", `No data found for sensorId: ${sensorId}`);
@@ -51,12 +51,12 @@ io.on("connection", (socket) => {
 });
 
 // Function to periodically check and emit smoke levels
-const checkSmokeLevels = async () => {
+const checkSmokeConcentration = async () => {
   for (const [socketId, sensorId] of Object.entries(clients)) {
     try {
       const lastReading = await prisma.lastReading.findUnique({
         where: { sensorId: sensorId },
-        select: { smokeLevel: true },
+        select: { smokeConcentration: true },
       });
 
       if (lastReading) {
@@ -70,7 +70,7 @@ const checkSmokeLevels = async () => {
 };
 
 // Set interval to check smoke levels every 10 seconds (10000 ms)
-setInterval(checkSmokeLevels, 3000);
+setInterval(checkSmokeConcentrations, 3000);
 
 server.listen(8080, () => {
   console.log("server running at http://localhost:8080");
